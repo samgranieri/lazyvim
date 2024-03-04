f9b35e5eaec21480eddc44c1bd5f3413bf97cbb7
Sam Granieri, Jr <sgranieri@thecitybase.com>
2024-03-04 12:39:32 -0600

    installing git-blame

---
+++ lua/plugins/git-blame.lua
@@ -0,0 +1,10 @@
+return {
+  "f-person/git-blame.nvim",
+  cmd = {
+    "GitBlameOpenCommitURL",
+    "GitBlameOpenFileURL",
+    "GitBlameCopyCommitURL",
+    "GitBlameCopyFileURL",
+    "GitBlameCopySHA",
+  },
+}

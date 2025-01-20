b71a6b6304d0b0065060723da44bcf6d5d469403
Sam Granieri, Jr <sam.granieri@eunasolutions.com>
2025-01-20 09:07:08 -0600

    update

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -4,3 +4,12 @@
 -- local convert_ruby_to_json = require("plugins.convert_ruby_to_json")
 
 -- Create a command to run the conversion
+if vim.fn.getenv("TERM_PROGRAM") == "ghostty" then
+	vim.opt.title = true
+	vim.opt.titlestring = "%{fnamemodify(getcwd(), ':t')}"
+end
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+	pattern = "*.json.jbuilder",
+	command = "set filetype=ruby",
+})
